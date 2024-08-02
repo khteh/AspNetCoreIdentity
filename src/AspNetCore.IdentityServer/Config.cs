5ac54db94443d5fd029e5c267058360e1d39e773
Kok How, Teh <funcoolgeek@gmail.com>
2024-08-02 16:14:27 +0800

    Update

---
+++ src/AspNetCore.IdentityServer/Config.cs
@@ -0,0 +1,73 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using IdentityModel;
+
+namespace AspNetCore.IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResource()
+            {
+                Name = "email_verification",
+                UserClaims = new List<string>
+                {
+                    JwtClaimTypes.Email,
+                    JwtClaimTypes.EmailVerified,
+                }
+            },
+            new IdentityResource()
+            {
+                Name = "phone_verification",
+                UserClaims = new List<string>
+                {
+                    JwtClaimTypes.PhoneNumber,
+                    JwtClaimTypes.PhoneNumberVerified,
+                }
+            }
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new ApiScope(name: "api1", displayName: "API1")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            new Client {
+                ClientId = "client1",
+                // no interactive user, use the clientId/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                // secret for authentication
+                ClientSecrets = { new Secret("client1secret".Sha512())},
+                // scopes that this client has access to.
+                AllowedScopes = {"api1"}
+            },
+            new Client {
+                ClientId = "openidclient",
+                ClientSecrets = { new Secret("openidsecret".Sha512())},
+                AllowedGrantTypes = GrantTypes.Code,
+                // Where to redirect to after login
+                RedirectUris = { "https://localhost:5002/signin-oidc"},
+                // Where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc"},
+                AllowOfflineAccess = true, // Enable support for refresh tokens
+                // IdentityServer will respond with two tokens
+                AllowedScopes = new List<string>
+                {
+                    // (1) identity token, containing information about the authentication process and session, and
+                    // (2) the access token, allowing access to APIs on behalf of the logged on user
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "api1", // so that the client will have permission to access it.
+                    "email_verification", "phone_verification"
+                }
+            }
+        };
+}
---
+++ src/AspNetCore.IdentityServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using AspNetCore.IdentityServer;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    var app = builder
+        .ConfigureServices()
+        .ConfigurePipeline();
+
+    app.Run();
+}
+catch (Exception ex) when (ex.GetType().Name is not "StopTheHostException") // https://github.com/dotnet/runtime/issues/60600
+{
+    // The Entity Framework CLI internally starts up IdentityServer for a short time in order to read your database configuration.
+    // After it has read the configuration, it shuts IdentityServer down by throwing a StopTheHostException exception.
+    // We expect this exception to be unhandled and therefore stop IdentityServer. Since it is expected, you do not need to log it as a fatal error. 
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
